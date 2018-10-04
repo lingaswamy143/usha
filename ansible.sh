9b3f1c42dbe1d7adff86c9d4ee9abba762f59a26
lingaswamy143 <lingaswamy2764@gmail.com>
2018-10-04 15:57:28 +0530

    modified changes

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,4 @@
-wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -ivh epel-release-latest-7.noarch.rpm
+sudo wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -P /home/ec2-user
+cd /home/ec2-user
+sudo rpm -ivh epel-release-latest-7.noarch.rpm
 sudo yum install ansible -y
